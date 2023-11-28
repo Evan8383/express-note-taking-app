0ce644f561b4134d75469987d072056bfc315c72
Evan <evanbennett83@gmail.com>
2023-11-27 23:02:36 -0500

    Added middleware. Converted get and post request to the same address to use route.

--- server.js
+++ server.js
@@ -14,29 +14,32 @@ const getNextId = (arr) => {
 }
 
 const updateNotes = async () => {
-  await fs.writeFile('./db/db.json', JSON.stringify(db))
+  await fs.writeFile('./db/db.json', JSON.stringify(db, null, 2))
 }
 
 app.use(express.static('public'));
+
 app.use(express.json());
+app.use(express.urlencoded({extended: true}))
 
 app.get('/notes', (req, res) => {
   res.sendFile(__dirname + '/public/notes.html')
 })
 
-app.get('/api/notes', async (req, res) => {
-  await updateNotes()
-  res.json(db)
-})
+app.route('/api/notes')
+  .get(async (req, res) => {
+    await updateNotes()
+    res.json(db)
+  })
 
-app.post('/api/notes', async (req, res) => {
-  const { title, text } = await req.body
-  let id = getNextId(db) || 0
+  .post(async (req, res) => {
+    const { title, text } = await req.body
+    let id = getNextId(db) || 1
+    const noteData = { id, title, text }
 
-  const noteData = { id, title, text }
-  db.push(noteData)
-  res.send('Note added')
-})
+    db.push(noteData)
+    res.send('Note added')
+  })
 
 app.delete('/api/notes/:id', (req, res) => {
   db.forEach((note, index) => {
