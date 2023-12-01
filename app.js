dbf5e998adfe496757efb722c1afb69c2db0ce54
Evan <evanbennett83@gmail.com>
2023-11-30 22:18:34 -0500

    added catch all route and removed test db item.

--- app.js
+++ app.js
@@ -13,5 +13,8 @@ app.get('/notes', (req, res) => {
   res.sendFile(__dirname + '/public/notes.html')
 })
 
+app.get('*', (req, res)=>{
+  res.sendFile(__dirname + '/public/index.html')
+})
 
 module.exports = app;
