7afae60e753c24ae2e99dcfc8941273b2789bdcc
Evan <evanbennett83@gmail.com>
2023-12-02 13:22:34 -0500

    removed console.log

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -163,7 +163,6 @@ const handleNoteDelete = (e) => {
 const handleNoteView = (e) => {
   e.preventDefault();
     activeNote = JSON.parse(e.target.parentElement.getAttribute('data-note'));
-    console.log(activeNote)
     renderActiveNote();
 };
 
